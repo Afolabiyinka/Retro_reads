f73ba78643be59427c6a3ab0372b04d75e87bc70
Ice Chain <afolabiyinka738@gmail.com>
2025-12-28 17:31:58 +0100

    Adding a netlfy json file

--- src/modules/nav/NavBar.tsx
+++ src/modules/nav/NavBar.tsx
@@ -10,12 +10,15 @@ const NavBar = () => {
       <Link to={`/`}>
         <div className="flex items-center p-2 gap-3 rounded-xl cursor-pointer">
           <Book />
-          <h1 className="text-2xl font-extrabold capitalize font-[Lora] tracking-widest">
+          <h1 className="text-xl font-extrabold capitalize font-[Lora] tracking-widest">
             RetroReads
           </h1>
         </div>
       </Link>
 
+      <Link className="md:text-xl cursor-pointer" to={`/books`}>
+        Books
+      </Link>
       {/* Action Buttons */}
       <div className="flex items-center gap-3">
         {/* Search Button */}
