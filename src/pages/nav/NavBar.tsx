8dfecdfa4daa7233f94901c81bf91e68ad8d9a9e
Ice Chain <afolabiyinka738@gmail.com>
2026-01-29 20:03:04 +0100

    Refactor NAVICONS import path and relocate nav.ts to the correct directory

--- src/pages/nav/NavBar.tsx
+++ src/pages/nav/NavBar.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import { Book } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
-import { NAVICONS } from "@/libs/nav";
+import { NAVICONS } from "@/lib/nav";
 import { useLocation } from "react-router-dom";
 
 const NavBar = () => {
   const location = useLocation();
   return (
     <nav className="w-full p-3 flex justify-between items-center shadow fixed z-50 bg-white">
-      {/* Logo Section */}
       <Link to={`/`}>
         <div className="flex items-center p-2 gap-3 rounded-xl cursor-pointer">
           <Book />
